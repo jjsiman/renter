2e6acbc606fe6ad43366ec71084f1e05d65c316c
John Siman <johnjsiman@gmail.com>
2024-06-10 03:16:25 +0000

    Update eslint and tsconfig

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,24 +1,27 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginVue from 'eslint-plugin-vue';
 import stylistic from '@stylistic/eslint-plugin';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  {
-    plugins: {
-      '@stylistic': stylistic,
-    },
-    languageOptions: { globals: globals.browser },
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
   stylistic.configs.customize({
     indent: 2,
     quotes: 'single',
     semi: true,
+    commaDangle: 'only-multiline',
   }),
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
   eslintConfigPrettier,
 ];
