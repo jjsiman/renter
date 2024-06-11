c153cf22c6750979ddfe6dcd0985a96fde077a0b
John Siman <johnjsiman@gmail.com>
2024-06-11 02:55:21 +0000

    Add Building model

--- backend/buildings/models.py
+++ backend/buildings/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Building(models.Model):
+    name = models.CharField(blank=False)
+    description = models.TextField(blank=True, default="")
+
+    address = models.CharField(default="")
+    city = models.CharField(default="")
+    state = models.CharField(default="")
+    zipcode = models.PositiveIntegerField(default=0)
+
+    units = models.PositiveSmallIntegerField(default=0)
+    stories = models.PositiveSmallIntegerField(default=0)
+    year_built = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.name
