d120b1e2ff4612fd7d9c9db527d50baeedaf7b2d
John Siman <johnjsiman@gmail.com>
2024-07-16 22:26:13 +0000

    Move index.html so vite can find public images

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,10 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   plugins: [vue()],
-  root: resolve(__dirname, 'src'),
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src/js', import.meta.url)),
