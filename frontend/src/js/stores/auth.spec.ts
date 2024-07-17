3f41be0044a926bac437ff61ef28acdc018d000e
John Siman <johnjsiman@gmail.com>
2024-07-17 02:57:22 +0000

    Fix failing auth test

--- frontend/src/js/stores/auth.spec.ts
+++ frontend/src/js/stores/auth.spec.ts
@@ -1,25 +1,17 @@
-import * as vueUse from '@vueuse/core';
 import { afterEach, beforeEach, expect, test, vi } from 'vitest';
 import { createPinia, setActivePinia } from 'pinia';
 import axios from '@/services/axios';
-import { ref } from 'vue';
 import { useAuth } from '@/stores/auth';
 
 vi.useFakeTimers();
 vi.setSystemTime(new Date(2024, 1, 1));
 
-vi.mock('@vueuse/core', () => ({
-  useStorage: vi.fn(),
-}));
-vi.mock('@/services/axios');
+const getItem = vi.spyOn(Storage.prototype, 'getItem');
 
-const useStorage = vi.spyOn(vueUse, 'useStorage');
+vi.mock('@/services/axios');
 
 beforeEach(() => {
   setActivePinia(createPinia());
-
-  // Provide empty default for any key
-  useStorage.mockImplementation(() => ref(''));
 });
 
 afterEach(() => {
@@ -52,28 +44,26 @@ test('logout issues post request to logout', () => {
 });
 
 test('isAuthenticated is false when token is absent', () => {
-  useStorage.mockImplementation(() => ref(''));
-
   const store = useAuth();
   expect(store.isAuthenticated).toBe(false);
 });
 
 test('isAuthenticated is true when token is present', () => {
-  useStorage.mockImplementation(() => ref('abc'));
+  getItem.mockImplementation(() => 'abc');
 
   const store = useAuth();
   expect(store.isAuthenticated).toBe(true);
 });
 
 test('token is cleared if expiry is in the past', () => {
-  useStorage.mockImplementationOnce(() => ref('abc')).mockImplementationOnce(() => ref('2010-01-01'));
+  getItem.mockImplementationOnce(() => 'abc').mockImplementationOnce(() => '2010-01-01');
 
   const store = useAuth();
   expect(store.isAuthenticated).toBe(false);
 });
 
 test('reset effectively logs out a user', () => {
-  useStorage.mockImplementation(() => ref('abc'));
+  getItem.mockImplementation(() => 'abc');
 
   const store = useAuth();
   expect(store.isAuthenticated).toBe(true);
